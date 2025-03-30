831aca6f45d7fcc96c538ce9b8869f4991d73689
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-30 15:06:52 +0530

    update css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
 import LinkPreviewer from './LinkPreviewer';
 
 function App() {
   return (
     <div className="container">
       
-      <LinkPreviewer url="https://www.naukri.com/mnjuser/profile">
-      <span className='link'>Naukari.com</span>
+      <LinkPreviewer url="https://en.wikipedia.org/wiki/Main_Page">
+      <span className='link'>wikipedia</span>
       </LinkPreviewer>
   
       <LinkPreviewer url="https://mayankmehta.vercel.app/">
-      <span className='link'>Linkeldn</span>
+      <span className='link'>portfolio</span>
       </LinkPreviewer>
       
       <LinkPreviewer url="https://github.com/mayankmehta19032003/Postify">
