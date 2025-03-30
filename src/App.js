c0d96f65f6017777eba4849ee8f3672d630fda8f
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-30 12:14:33 +0530

    add func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import LinkPreviewer from './LinkPreviewer';
 
 function App() {
   return (
     <div className="container">
       
+      <LinkPreviewer url="https://www.naukri.com/mnjuser/profile">
+      <span className='link'>Naukari.com</span>
+      </LinkPreviewer>
+  
+      <LinkPreviewer url="https://mayankmehta.vercel.app/">
+      <span className='link'>Linkeldn</span>
+      </LinkPreviewer>
+      
+      <LinkPreviewer url="https://github.com/mayankmehta19032003/Postify">
+      <span className='link'>Github</span>
+      </LinkPreviewer>
+     
     </div>
   );
 }
